67200d5db7d2274afa1662290e4ed4364adec342
DSM2 <fatec-dsm2@academico.intra>
2025-08-07 19:21:18 -0300

    add functions and var

--- Javascript/01.1/js.01.1.js
+++ Javascript/01.1/js.01.1.js
@@ -1,9 +1,39 @@
+//Variáveis podem ser declaradas de 3 formas:
+//Var, Let e Const
+//Var: no geral evite o seu uso, pode não ser muito seguro.
+//Let: utilize quando for necessário reatribuir o valor da variável.
+//Const: utilize quando não for necessário reatribuir o valor.
+
+// var nome = "Diego";
+// var nome = "Maria";
+// let cidade = "Registro";
+// let cidade = "Pariquera"; // NÃO PODE
+// let endereco
+// endereco = "Rua tall"
+// endereco = "lolo"
+// const idade // NÃO PODE
+// const idade = 18
+// idade = 20 //NÃO PODE
+
 // TIPOS DE FUNÇÕES
 
 //FUNÇÃO SIMPLES
-function showMessage(){
-    document.write("<h2>Olá! Bem-Vindo! Essa é sua primeira função!</h2>")
+const message = "<h2>Olá! Bem-Vindo! Essa é sua primeira função!</h2>";
+function showMessage() {
+  document.write(message);
 }
 
-// INVOCANDO A FUNÇÃO    
+// INVOCANDO A FUNÇÃO
 showMessage();
+
+// FUNÇÃO COM PARÂMETROS
+const user = "Ana Flávia";
+
+function userMessage(user) {
+  //Essa função recebe um parâmetro
+  document.write(`<h3>O que deseja fazer hoje, ${user} ?</h3>`);
+  // ${} -> template strings / literal strings
+  // É usado para inserir variáveis dentro de STRINGS aspas
+}
+
+userMessage(user);
