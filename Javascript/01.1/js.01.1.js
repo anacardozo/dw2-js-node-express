63e9dc8644e86a53a12f1082ce7d7a557b81813f
DSM2 <fatec-dsm2@academico.intra>
2025-08-06 21:26:58 -0300

    @anacardozo

---
+++ Javascript/01.1/js.01.1.js
@@ -0,0 +1,9 @@
+// TIPOS DE FUNÇÕES
+
+//FUNÇÃO SIMPLES
+function showMessage(){
+    document.write("<h2>Olá! Bem-Vindo! Essa é sua primeira função!</h2>")
+}
+
+// INVOCANDO A FUNÇÃO    
+showMessage();
