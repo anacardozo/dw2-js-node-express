43b811b0886084ad9ef7b7c65601e2ad1edc3a52
DSM2 <fatec-dsm2@academico.intra>
2025-10-08 20:48:40 -0300

    crud com mysql

--- Node.js/nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
+++ Node.js/nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
@@ -6,11 +6,68 @@ import Cliente from "../models/Cliente.js";
 // ROTA CLIENTES
 router.get("/clientes", function (req, res) {
   //Select * from clientes
-  Cliente.findAll().then((clientes) => {
-    res.render("clientes", {
-      clientes: clientes,
+  Cliente.findAll()
+    .then((clientes) => {
+      // then => promessa bem sucedida
+      res.render("clientes", {
+        clientes: clientes,
+      });
+      //catch => falha na resolução da promessa
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
 });
+
+// ROTA DE CADASTRO DE CLIENTES
+router.post("/clientes/new", (req, res) => {
+  //coletando os dados do formulário
+  const nome = req.body.nome;
+  const cpf = req.body.cpf;
+  const endereco = req.body.endereco;
+  Cliente.create({
+    //coluna : dado do form
+    nome: nome,
+    cpf: cpf,
+    endereco: endereco,
+  })
+    .then(() => {
+      // se der certo redireciona para a pagina clientes
+      res.redirect("/clientes");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//ROTA DE EXCLUSÃO DO CLIENTE
+//:id é um parâmetro obrigatório
+router.get("/clientes/delete/:id", (req, res) => {
+  const id = req.params.id;
+  //.destroy() -> exclui um registro do banco
+  Cliente.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.redirect("/clientes");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//ROTA DE EDIÇÃO DE CLIENTES
+router.get("/clientes/edit/:id", (req, res) => {
+  const id = req.params.id
+  //Buscando o cliente pela ID
+  // findByPk() -> busca um regsitro pela chave primária (id)
+  Cliente.findByPk(id).then(cliente => {
+    res.render("clienteEdit", {
+      cliente : cliente
+    })
+  })
+})
 
 export default router;
