f822efe9a57d68f85a219480514e152d6a395515
DSM2 <fatec-dsm2@academico.intra>
2025-10-29 21:34:03 -0300

    aula 07

--- Node.js/nodejs-aula-07-Login- e - Segurança - Copia/controllers/UsersController.js
+++ Node.js/nodejs-aula-07-Login- e - Segurança - Copia/controllers/UsersController.js
@@ -1,32 +1,93 @@
 import express from "express";
-const router = express.Router()
+const router = express.Router();
 //Importando o model de usuário
 import User from "../models/User.js";
+//Importando o bcrypt
+import bcrypt from "bcrypt";
+import { where } from "sequelize";
 
 //Rota de Login
 router.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 //Rota de Cadastro
 router.get("/cadastro", (req, res) => {
-    res.render("cadastro");
+  res.render("cadastro");
 });
 
 //Rota de Criação de Usuário
 router.post("/createUser", (req, res) => {
-    //Coletando os dados do formulario
-    const email = req.body.email
-    const password = req.body.password
-    //Enviando para o banco
-    User.create({
+  //Coletando os dados do formulario
+  const email = req.body.email;
+  const password = req.body.password;
+  //Verificando se o usuario ja esta cadastrado no banco
+  User.findOne({ where: { email: email } }).then((user) => {
+    //Seo usuario não exisitir
+    if (user == undefined) {
+      //FARÁ o cadastro
+      //Gerando o hash de senha
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+
+      //Enviando para o banco
+      User.create({
         email: email,
-        password: password
-    }).then(() => {
-        res.redirect("/login");
-    }).catch(error => {
-        console.log(error)
-    })
-})
+        password: hash,
+      })
+        .then(() => {
+          res.redirect("/login");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      //Se o usuario Já exisitir
+    } else {
+      res.send(`O usuário informado já esta cadastrado! <br>
+            <a href="/login">Tentar Novamente.</a>`);
+    }
+  });
+});
+
+//Rota de Autenficação
+router.post("/authenticate", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  //Busca o usuario no banco
+  User.findOne({ where: { email: email } }).then((user) => {
+    //Se o usuario existir
+    if (user != undefined) {
+      //Validação da senha
+      const correct = bcrypt.compareSync(password, user.password);
+      //Se a senha for valida
+      if (correct) {
+        //Autoriza o Login
+        //Gerando a sessão para o usuario
+        req.session.user = {
+          id: user.id,
+          email: user.email,
+        };
+        // res.send(`Usuário logado é: <br>
+        //     ID: ${req.session.user["id"]}<br>
+        //     E-mail: ${req.session.user["email"]}`);
+        res.redirect("/");
+        //Se a senha não for valida
+      } else {
+        res.send(`A senha digitada está incorreta! <br> 
+            <a href="/login">Tentar novamente</a>`);
+      }
+      //se o usuario não existir
+    } else {
+      res.send(`Usuário informado não existe! <br>
+            <a href="/login">Tentar novamente</a>`);
+    }
+  });
+});
+
+//Rota de Logout
+router.get("/logout", (req, res) => {
+  req.session.user = undefined;
+  res.redirect("/");
+});
 
 export default router;
--- Node.js/nodejs-aula-07-Login- e - Segurança - Copia/index.js
+++ Node.js/nodejs-aula-07-Login- e - Segurança - Copia/index.js
@@ -1,71 +1,88 @@
 // Importando o Express
-import express from "express" 
+import express from "express";
 // Iniciando o Express
-const app = express() 
-// Importando o Sequelize
-import connection from "./config/sequelize-config.js" 
-// Importando os Controllers
-import ClientesController from "./controllers/ClientesController.js"
-import PedidosController from "./controllers/PedidosController.js"
-import ProdutosController from "./controllers/ProdutosController.js"
-//Importando o Controller de Usuário
-import UsersController from "./controllers/UsersController.js"
+const app = express();
+//Importando o express session (gerador de sessões)
+import session from "express-session";
 
+// Importando o Sequelize
+import connection from "./config/sequelize-config.js";
+// Importando os Controllers
+import ClientesController from "./controllers/ClientesController.js";
+import PedidosController from "./controllers/PedidosController.js";
+import ProdutosController from "./controllers/ProdutosController.js";
+//Importando o Controller de Usuário
+import UsersController from "./controllers/UsersController.js";
 
 //Importando os Models
 import Cliente from "./models/Cliente.js";
-import Pedido from "./models/Pedido.js"
+import Pedido from "./models/Pedido.js";
 //Importando os relacionamentos
-import defineAssociations from "./config/associations.js"
+import defineAssociations from "./config/associations.js";
+
+//Configurando o Express Session (gerando um segredo para a sessão do site
+app.use(
+  session({
+    secret: "minhalojasecret",
+    cookie: { maxAge: 3600000 }, //sessão expira em uma hora -> tempo que a sessão dura
+    saveUninitialized: false,
+    resave: false, //não reescreve a sessão
+  })
+);
 
 // Realizando a conexão com o banco de dados
-connection.authenticate().then(()=> {
-    console.log("Conexão com o banco de dados feita com sucesso!")
-}).catch((error) => {
-    console.log(error)
-})
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão com o banco de dados feita com sucesso!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Criando o banco de dados se ele não existir
-connection.query(`CREATE DATABASE IF NOT EXISTS lojarelacional;`).then(() => {
-    console.log("O banco de dados está criado.")
-}).catch((error) => {
-    console.log(error)
-})
+connection
+  .query(`CREATE DATABASE IF NOT EXISTS lojarelacional;`)
+  .then(() => {
+    console.log("O banco de dados está criado.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Invocando a função que cria os relacionamentos
-defineAssociations()
-Promise.all([
-Cliente.sync({force:false}),
-Pedido.sync({force:false}),
-]).then(() => {
-    console.log("Tabelas e relacionamentos criados com sucesso!")
-}).catch(error => {
-    console.log("Erro na criação de tabelas: ", error)
-});
+defineAssociations();
+Promise.all([Cliente.sync({ force: false }), Pedido.sync({ force: false })])
+  .then(() => {
+    console.log("Tabelas e relacionamentos criados com sucesso!");
+  })
+  .catch((error) => {
+    console.log("Erro na criação de tabelas: ", error);
+  });
 
 // Define o EJS como Renderizador de páginas
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 // Define o uso da pasta "public" para uso de arquivos estáticos
-app.use(express.static('public'))
+app.use(express.static("public"));
 // Permite capturar dados vindo de formulários
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 // Definindo o uso das rotas dos Controllers
-app.use("/", ClientesController)
-app.use("/", PedidosController)
-app.use("/", ProdutosController)
-app.use("/", UsersController)
+app.use("/", ClientesController);
+app.use("/", PedidosController);
+app.use("/", ProdutosController);
+app.use("/", UsersController);
 
 // ROTA PRINCIPAL
-app.get("/", function(req,res){
-    res.render("index")
-})
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
 // INICIA O SERVIDOR NA PORTA 8080
-app.listen(8080, erro => {
-    if (erro) {
-        console.log("Ocorreu um erro!")
-    } else {
-        console.log("Servidor iniciado com sucesso!")
-    }
-})
+app.listen(8080, (erro) => {
+  if (erro) {
+    console.log("Ocorreu um erro!");
+  } else {
+    console.log("Servidor iniciado com sucesso!");
+  }
+});
